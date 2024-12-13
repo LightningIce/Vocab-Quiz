7f6654501457e715dfcf9c9b1901a4a77dfc214a
unknown <lightningiceivan@gmail.com>
2024-12-13 16:18:26 +0800

    updated adminheader.php

--- php/adminheader.php
+++ php/adminheader.php
@@ -1,26 +1,31 @@
 <header class="admin-header">
 
-        <div class="nav-wrap" id="nav-border-hack">
-            <div id="header-logo">
-                <div class="header-logo-cat"><a href="adminhome.php"><img src="../images/icon.png" alt="Logo"></a></div>
-                <div class="header-logo-name"><a href="adminhome.php">Vocab Quiz</a></div>
-            </div>
-            <div id="header-navigation">
-                <ul>
-                    <li class="nav-home"><a href="adminhome.php"><span>Home</span></a></li>
-                    <li class="nav-easy"><a href="admindashboardeasy.php"><span>Easy</span></a></li>
-                    <li class="nav-hard"><a href="admindashboardhard.php"><span>Hard</span></a></li>
-                    <li class="nav-business"><a href="admindashboardbusiness.php"><span>Business</span></a></li>
-                </ul>
-            </div>
-            <div id="header-create-quiz">
-                <div class="header-create-button"><a href="adminquizadd.php"></a></div>
-            </div>
+    <div class="nav-wrap" id="nav-border-hack">
+        <div id="header-logo">
+            <div class="header-logo-cat"><a href="adminhome.php"><img src="../images/icon.png" alt="Logo"></a></div>
+            <div class="header-logo-name"><a href="adminhome.php">Vocab Quiz</a></div>
+        </div>
+        <div id="header-navigation">
+            <ul>
+                <li class="nav-home"><a href="adminhome.php"><span>Home</span></a></li>
+                <li class="nav-easy"><a href="admindashboardeasy.php"><span>Easy</span></a></li>
+                <li class="nav-hard"><a href="admindashboardhard.php"><span>Hard</span></a></li>
+                <li class="nav-business"><a href="admindashboardbusiness.php"><span>Business</span></a></li>
+            </ul>
+        </div>
+        <div id="header-create-quiz">
+            <div class="header-create-button"><a href="adminquizadd.php"><span>Create Quiz</span></a></div>
+        </div>
+        <div class="admin-dropdown-container">
+            <button class="admin-dropdown-button">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+                <i class="fa fa-times" aria-hidden="true"></i></button>
             <div class="admin-dropdown">
                 <ul class="admin-dropdown-items">
-                    <li><a href="allaccount.php"><span>Profile</span></a></li>
-                    <li><a href="alllogin.php"><span>Log Out</span></a></li>
+                    <li><a href="allaccount.php"><span class=admin-dropdown-profile-icon><i class="fa fa-user" aria-hidden="true"></i></span><span>Profile</span></a></li>
+                    <li><a href="alllogin.php"><span><i class="fa fa-sign-out" aria-hidden="true"></i></span><span>Log Out</span></a></li>
                 </ul>
             </div>
         </div>
+    </div>
 </header>
--- php/adminhome.php
+++ php/adminhome.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Vocab Quiz Dashboard for Admin">
     <title>Vocab Quiz Dashboard</title>
-<link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 </head>
+
 <body>
     <?php include 'adminheader.php'; ?>
     <main></main>
     <?php include 'adminfooter.php'; ?>
 </body>
+
 </html>
