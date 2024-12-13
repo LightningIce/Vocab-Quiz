5bf773a65956ecfdb0823f7ce10c3920504da100
unknown <lightningiceivan@gmail.com>
2024-12-13 08:32:02 +0800

    added admin header and footer

---
+++ php/adminheader.php
@@ -0,0 +1,19 @@
+<header class="admin-header">
+    <nav class="main-nav">
+        <div class="nav-wrap" id="nav-border-hack">
+            <div id="header-logo">
+                <div class="header-logo-cat"><a href="adminhome.php"><img src="../images/icon.png" alt="Logo"></a></div>
+                <div class="header-logo-name"><a href="adminhome.php"></a></div>
+            </div>
+            <div id="header-navigation">
+                <ul>
+                    <li class="nav-home"><a href="adminhome.php"><span>Home</span></a></li>
+                    <li class="nav-easy"><a href="admindashboardeasy.php"><span>Easy</span></a></li>
+                    <li class="nav-hard"><a href="admindashboardhard.php"><span>Hard</span></a></li>
+                    <li class="nav-business"><a href="admindashboardbusiness.php"><span>Business</span></a></li>
+                </ul>
+            </div>
+            <div id="header-new-quiz"><div class="header-create-button"><a href="adminquizadd.php"></a></div></div>
+        </div>
+    </nav>
+</header>
