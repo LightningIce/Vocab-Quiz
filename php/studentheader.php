63c306c2b78a8d009e75d85d676ede4b1ed576cc
Leong1104 <hinchung0152@gmail.com>
2024-12-16 15:23:27 +0800

    Update studentheader.php

--- php/studentheader.php
+++ php/studentheader.php
@@ -1,27 +1,28 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-<header>
-    <nav>
-        <div class="logo">
-            <img src="../images/icon.png" alt="VocabQuiz Logo">
-            VocabQuiz
-        </div>
-        <ul class="nav-links">
-            <li><a href="studenthomepage.php">Home</a></li>
-            <li><a href="studentdashboard.php">Quizzes</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-        <div class="profile">
-            <div class="profile-toggle" onclick="toggleProfileMenu()">
-                <i class="fas fa-user" aria-hidden="false"></i>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="../images/icon.png" alt="VocabQuiz Logo">
+                VocabQuiz
             </div>
-            <div class="profile-dropdown">
-                <a href="#profile">My Profile</a>
-                <a href="studentquizhistory.php">Quiz History</a>
-                <a href="#settings">Settings</a>
-                <a href="logout.php">Logout</a>
-            </div>
-        </div>
-    </nav>
-</header>
+            <ul class="nav-links">
+                <li><a href="HomePage.html">Home</a></li>
+                <li><a href="#quizzes">Quizzes</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="profile">
+            
+                    <div class="profile-toggle" onclick="toggleProfileMenu()">
+                        <img src="../images/Profile_Icon.png" alt="Profile" class="profile-icon">
+                    </div>
+                    <div class="profile-dropdown">
+                        <a href="#profile">My Profile</a>
+                        <a href="#history">Quiz History</a>
+                        <a href="#settings">Settings</a>
+                        <a href="#logout">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
